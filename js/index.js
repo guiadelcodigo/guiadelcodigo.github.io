59b5448b31acc353bc4d6a2086bbfa86f825e37b
guiadelcodigo <guiadelcodigo@gmail.com>
2023-09-16 16:13:56 -0500

    cambiando organizacion de archivos y codigos

---
+++ js/index.js
@@ -0,0 +1,4 @@
+        const icon = document.getElementById('icon'); 
+        const aside = document.getElementById('aside');
+
+icon.addEventListener('click', () => { aside.classList.toggle('active'); }); 
